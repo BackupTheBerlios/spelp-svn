527558c5c94cf8b20a6bca8b9c4a6cd2e5102423
toine <toine@481c8688-1820-0410-a890-ee3dbf266800>
2006-10-27 15:17:50 +0000

    MAJ jar

--- BRANCHES/FAISABILITE/src/parser/testUnMarshall.java
+++ BRANCHES/FAISABILITE/src/parser/testUnMarshall.java
@@ -26,15 +26,27 @@ public class testUnMarshall {
 	            JAXBElement<?> el = 
 	                (JAXBElement<?>)u.unmarshal(new File("toyton.xml"));
 	            MethodLibrary mlb = (MethodLibrary)el.getValue();
-	            
-	            
-	            MethodLibrary ml = new MethodLibrary();
+	           MethodPlugin mp;
 	           
-	           
+	            
 	           
 	            List<MethodPlugin> l = mlb.getMethodPlugin();
+	            List<MethodPackage> l2 ;
+	            List<Object> l3 ;
 	            for (int i = 0; i< l.size(); i++) {
 	            	System.out.println(l.get(i).getName());
+	            	mp = l.get(i);
+	            	l2 = mp.getMethodPackage();
+	            	 for (int y = 0; y< l2.size(); y++) {
+	            		 System.out.println("	"+l2.get(y).getName());
+	            		 l3 = l2.get(y).getReusedPackageOrMethodPackage();
+	            		 for (int z = 0; z< l3.size(); z++) {
+	            			 System.out.println("XXX		"+((MethodPackage)l3.get(z)).getName());
+	            		 }
+	            	 }
+	            	
+
+	            	
 	            }
 	              
 	        } catch( Exception e ) {
