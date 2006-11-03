87687af8ed5aa61ca68696947d1a13cbb619d499
toine <toine@481c8688-1820-0410-a890-ee3dbf266800>
2006-11-03 13:40:51 +0000

    signature des methodes de la classe Parser (suite)

--- BRANCHES/FAISABILITE/src/parser/Parser.java
+++ BRANCHES/FAISABILITE/src/parser/Parser.java
@@ -19,10 +19,13 @@ public class Parser {
 
 	private static String FileXML = "toyton.xml";
 	private String roleDescriptor = "//BreakdownElement[@*[namespace-uri() and local-name()='type']='uma:RoleDescriptor']";
+	private String taskDescriptor = "//BreakdownElement[@*[namespace-uri() and local-name()='type']='uma:TaskDescriptor']";
 	private static Parser instance;
 	private FileInputStream url;
 
-    /* private constructor for the singleton */
+    /**
+     *  private constructor for the singleton 
+     */
 	private Parser() {
 		try {
 			url = new FileInputStream(FileXML);
@@ -32,7 +35,10 @@ public class Parser {
 		}
 	}
 
-	/* Singleton */
+	/**
+	 *  Singleton 
+	 *  @return the instance of Parser
+	 */
 	public static Parser getInstance() {
         if (null == instance) { // Premier appel
             instance = new Parser();
@@ -40,6 +46,12 @@ public class Parser {
         return instance;
     }
 
+	/**
+	 * function evaluate
+	 * @param stream
+	 * @param expression
+	 * @return
+	 */
 	public static Node evaluate(InputStream stream, String expression){
         Node node = null;
         try{
@@ -54,12 +66,18 @@ public class Parser {
         XPathExpression exp = xpath.compile(expression);
         node = (Node)exp.evaluate(source,XPathConstants.NODE);
         
-        }catch(XPathExpressionException xpee){
-        xpee.printStackTrace();
+        }catch(XPathExpressionException xpee){        	
+        	xpee.printStackTrace();        
         }
         return node;
         }
 	
+	/**
+	 * function evaluateNodeList
+	 * @param stream
+	 * @param expression
+	 * @return
+	 */
 	public static NodeList evaluateNodeList(InputStream stream, String expression){
         NodeList liste = null;
         try{
@@ -72,30 +90,52 @@ public class Parser {
         
         //evaluate Xpath expression
         XPathExpression exp = xpath.compile(expression);
+        System.out.println(exp.toString());
         liste = (NodeList)exp.evaluate(source,XPathConstants.NODESET);
         
         }catch(XPathExpressionException xpee){
         xpee.printStackTrace();
+        System.out.println("probleme dans le XPathExpression");
         }
         return liste;
         }
 
+	/**
+	 * function getFileXML
+	 * @return
+	 */
 	public static String getFileXML() {
 		return FileXML;
 	}
 
+	/**
+	 * procedure setFileXML
+	 * @param fileXML
+	 */
 	public static void setFileXML(String fileXML) {
 		FileXML = fileXML;
 	}
 
+	/**
+	 * function getUrl
+	 * @return
+	 */
 	public FileInputStream getUrl() {
 		return url;
 	}
 
+	/**
+	 * procedure setUrl
+	 * @param url
+	 */
 	public void setUrl(FileInputStream url) {
 		this.url = url;
 	}
 	
+	/**
+	 * function getRole
+	 * @return Arraylist
+	 *  */
 	public ArrayList getRole() {
 		ArrayList<String> roleList = new ArrayList<String>();
 		NodeList nodel = evaluateNodeList(url,roleDescriptor);
@@ -108,6 +148,42 @@ public class Parser {
 	        roleList.add(role);
 		}		
 		return roleList;
+	}
+	
+	/**
+	 * function getPrimaryTaskByRole
+	 * @return Arraylist
+	 * @param String roleName
+	 *  */	
+	public ArrayList getPrimaryTaskByRole(String roleName) {
+		ArrayList<String> primaryTask = new ArrayList<String>();
+		NodeList nodel = evaluateNodeList(url, taskDescriptor);
+		Node node;
+		String task;
+		for(int i=0;i<nodel.getLength();i++){
+			node = nodel.item(i);
+			task = node.getAttributes().getNamedItem("name").getNodeValue();
+			primaryTask.add(task);
+		}
+		
+		return primaryTask;
+	}
+
+	/**
+	 * function getTask
+	 * @return Arraylist
+	 *  */
+	public ArrayList getTask() {
+		ArrayList<String> taskList = new ArrayList<String>();
+		NodeList nodel = evaluateNodeList(url, taskDescriptor);
+		Node node;
+		String task;
+		for(int i=0;i<nodel.getLength();i++){
+			node = nodel.item(i);
+			task = node.getAttributes().getNamedItem("name").getNodeValue();
+			taskList.add(task);
+		}
+		return taskList;
 	}
 	
 }
--- BRANCHES/FAISABILITE/src/parser/TestParser.java
+++ BRANCHES/FAISABILITE/src/parser/TestParser.java
@@ -10,10 +10,12 @@ public class TestParser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Parser p = Parser.getInstance();
-		ArrayList role = p.getRole();
+		//ArrayList role = p.getRole();
+		ArrayList task = p.getTask();
 		
-		for(int i=0;i<role.size();i++) {
-			System.out.println(role.get(i));
+		for(int i=0;i<task.size();i++) {
+			//System.out.println(role.get(i));
+			System.out.println("	"+task.get(i));
 		}
 		
 	}
