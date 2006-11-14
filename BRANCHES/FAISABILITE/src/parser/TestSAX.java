6eaed2c6e411271f69ffe4358a109929d83332fd
toine <toine@481c8688-1820-0410-a890-ee3dbf266800>
2006-11-14 21:49:07 +0000

    test SAX et test DOM4J

--- BRANCHES/FAISABILITE/src/parser/TestSAX.java
+++ BRANCHES/FAISABILITE/src/parser/TestSAX.java
@@ -1,24 +1,101 @@
 package parser;
 
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-public class TestSAX {
+public class TestSAX extends DefaultHandler
+{
 
-	/**
-	 * @param args
-	 * @throws SAXException 
-	 */
-	public static void main(String[] args) throws SAXException, IOException {
-		// TODO Auto-generated method stub
+    public static void main (String args[])
+	throws Exception
+    {
+	XMLReader xr = XMLReaderFactory.createXMLReader();
+	TestSAX handler = new TestSAX();
+	xr.setContentHandler(handler);
+	xr.setErrorHandler(handler);
 
-		XMLReader saxReader = XMLReaderFactory.createXMLReader();
+				// Parse each file provided on the
+				// command line.
+	//for (int i = 0; i < args.length; i++) {
+		
+	    FileReader r = new FileReader("sortieEPF.xml");
+	    xr.parse(new InputSource(r));
+	//}
+    }
 
-	    
-	    saxReader.parse("sortieEPF.xml");
-		saxReader.getContentHandler().startDocument();
+
+    public TestSAX ()
+    {
+	super();
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+    // Event handlers.
+    ////////////////////////////////////////////////////////////////////
+
+
+    public void startDocument ()
+    {
+	System.out.println("Start document");
+    }
+
+
+    public void endDocument ()
+    {
+	System.out.println("End document");
+    }
+
+
+    public void startElement (String uri, String name,
+			      String qName, Attributes atts)
+    {
+	if ("".equals (uri))
+	    System.out.println("Start element: " + qName);
+	else
+	    System.out.println("Start element: {" + uri + "}" + name);
+    }
+
+
+    public void endElement (String uri, String name, String qName)
+    {
+	if ("".equals (uri))
+	    System.out.println("End element: " + qName);
+	else
+	    System.out.println("End element:   {" + uri + "}" + name);
+    }
+
+
+    public void characters (char ch[], int start, int length)
+    {
+	System.out.print("Characters:    \"");
+	for (int i = start; i < start + length; i++) {
+	    switch (ch[i]) {
+	    case '\\':
+		System.out.print("\\\\");
+		break;
+	    case '"':
+		System.out.print("\\\"");
+		break;
+	    case '\n':
+		System.out.print("\\n");
+		break;
+	    case '\r':
+		System.out.print("\\r");
+		break;
+	    case '\t':
+		System.out.print("\\t");
+		break;
+	    default:
+		System.out.print(ch[i]);
+		break;
+	    }
 	}
+	System.out.print("\"\n");
+    }
 
 }
--- BRANCHES/FAISABILITE/src/parser/Testdom4J.java
+++ BRANCHES/FAISABILITE/src/parser/Testdom4J.java
@@ -11,8 +11,10 @@ import java.util.List;
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.Node;
+import org.dom4j.XPath;
 import org.dom4j.io.SAXReader;
 
 
@@ -31,7 +33,7 @@ public class Testdom4J {
 		// TODO Auto-generated method stub
 		InputStream is = null;
 		try {
-			is = new FileInputStream("sortieEPF.xml");
+			is = new FileInputStream("essai.xml");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -41,17 +43,29 @@ public class Testdom4J {
 				Document document = reader.read(is);
 				
 				Element root = document.getRootElement();
-
 				
-				List list = document.selectNodes("");
-
-		        for (Iterator iter = list.iterator(); iter.hasNext(); ) {
-		        	Attribute node = (Attribute) iter.next();
-		            
-		            System.out.println(node.getValue());
-		        }
-				System.out.println(root);
+				System.out.println(root.nodeCount());
 				
+				String hobby = document.valueOf("/toine/essai[@name='toine']/hobby/@name");
+				System.out.println("hobby: "+hobby);
+				
+				/*root = document.getRootElement();
+			    Iterator elementIterator = root.elementIterator();
+			    while(elementIterator.hasNext()){
+			      Element element = (Element)elementIterator.next();
+			      System.out.println(element.getName());
+			    }*/
+				
+			    
+			    /*XPath xpathSelector = DocumentHelper.createXPath("/toine/essai[@name='toine']/hobby");
+				List results = xpathSelector.selectNodes(document);
+				for ( Iterator iter = results.iterator(); iter.hasNext();)
+				{
+				  Element element = (Element) iter.next();
+				  System.out.println(element.getName());
+				}*/
+			    
+			     
 				/* List list = document.selectNodes("//BreakdownElement");
 				
 				 for (Iterator iter = list.iterator(); iter.hasNext(); ) {
@@ -59,6 +73,7 @@ public class Testdom4J {
 			            String val = attribute.getValue();
 			            System.out.println(val);
 			        } */ 
+			    //String role = document.valueOf("//BreakdownElement[@name='developer']");
 
 			} catch (DocumentException e) {
 				// TODO Auto-generated catch block
