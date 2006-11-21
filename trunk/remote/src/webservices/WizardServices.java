2849b56c81433b4eb0d80be90b7cda4e577ad173
castel <castel@481c8688-1820-0410-a890-ee3dbf266800>
2006-11-21 13:54:47 +0000

    mise a jour du jar woops2 + ajout d'une classe WizardServices

---
+++ trunk/remote/src/webservices/WizardServices.java
@@ -0,0 +1,15 @@
+package webservices;
+
+import java.util.Collection;
+
+import woops2.business.activity.ActivityManager;
+import woops2.hibernate.role.RoleDescriptorDao;
+import woops2.model.role.RoleDescriptor;
+
+public class WizardServices {
+	public Collection<RoleDescriptor> getAllRoles () {
+
+
+		return null;
+	}	
+}
