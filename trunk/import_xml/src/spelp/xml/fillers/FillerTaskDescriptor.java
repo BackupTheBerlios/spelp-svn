40a9aa98bdd15228da508bb5bae16c2296af67e3
jf_doya <jf_doya@481c8688-1820-0410-a890-ee3dbf266800>
2006-11-22 12:28:40 +0000

    correction pour être correct par rapport au modèle

--- trunk/import_xml/src/spelp/xml/fillers/FillerTaskDescriptor.java
+++ trunk/import_xml/src/spelp/xml/fillers/FillerTaskDescriptor.java
@@ -2,11 +2,11 @@ package spelp.xml.fillers;
 
 import org.w3c.dom.Node;
 
-import woops2.model.breakdownelement.BreakdownElement;
+import woops2.model.task.TaskDescriptor;
 
 public class FillerTaskDescriptor extends FillerWorkBreakDownElement {
 
-	public FillerTaskDescriptor(BreakdownElement _e, Node _aNode) {
+	public FillerTaskDescriptor(TaskDescriptor _e, Node _aNode) {
 		super(_e, _aNode);
 	}
 
