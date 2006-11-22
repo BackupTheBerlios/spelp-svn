76f394b022464c1dd8b268020f5542e25499f479
faure <faure@481c8688-1820-0410-a890-ee3dbf266800>
2006-11-22 10:18:53 +0000

    modif XMLParserTest avec prise en compte de tous les fichiers XML du dossiers

--- trunk/import_xml/src/spelp/test/xml/parser/XMLParserTest.java
+++ trunk/import_xml/src/spelp/test/xml/parser/XMLParserTest.java
@@ -16,45 +16,53 @@ import woops2.model.task.TaskDefinition;
 import woops2.model.task.TaskDescriptor;
 
 public class XMLParserTest {
-	public static String path = "ressources" + File.separator + "scrum.xml"; 
-	
+	public static File pathScrum = new File("ressources" + File.separator + "scrum.xml"); 
+	public static File pathOPenUP =new File( "ressources" + File.separator + "sortieEPF.xml");
 	@Test
 	public void testGetProcess(){
-		File f = new File(path);
-		Process p = XMLParser.getProcess(f);
-		assertTrue(p.getBreakDownElements().size() != 0);
+		File[] f = new File[]{
+				pathScrum,pathOPenUP
+		};
+		for (int i = 0 ; i < f.length ; i++){
+			Process p = XMLParser.getProcess(f[i]);
+			assertTrue(p.getBreakDownElements().size() != 0);
+		}
 	}
 	
 	@Test
 	public void testGetAllTaskDescriptors() {
-		File f = new File(path);
-		XMLParser.setFile(f);
-		System.out.println(f.getAbsolutePath());
-		try {
-			Vector <TaskDefinition> v = new Vector<TaskDefinition>();
-			Set<TaskDescriptor> aSet = XMLParser.getAllTaskDescriptors();
-			// test if the set of tasks is filled
-			assertTrue(aSet.size() != 0);
-			// test if all task are unique
-			for (Iterator i = aSet.iterator() ; i.hasNext() ; ){
-				TaskDefinition t = ((TaskDescriptor)i.next()).getTaskDefinition();
-				if (t!=null){
-					for (int j = 0 ; j < v.size() ; j++){
-						System.out.println("\nID : \n" + t.getId() + "\n" + ((TaskDefinition)v.get(j)).getId() + "\nMemes ref : " + (t == ((TaskDefinition)v.get(j))));
-						assertTrue(
-							t.getId().equals(((TaskDefinition)v.get(j)).getId()) && t == ((TaskDefinition)v.get(j))
-							||
-							!t.getId().equals(((TaskDefinition)v.get(j)).getId())
-						);
-					}
-					if (!v.contains(t)){
-						v.add(t);
+		File[] f = new File[]{
+			pathScrum,pathOPenUP
+		};
+		for (int p = 0 ; p < f.length ; p++){
+			XMLParser.setFile(f[p]);
+			System.out.println(f[p].getAbsolutePath());
+			try {
+				Vector <TaskDefinition> v = new Vector<TaskDefinition>();
+				Set<TaskDescriptor> aSet = XMLParser.getAllTaskDescriptors();
+				// test if the set of tasks is filled
+				assertTrue(aSet.size() != 0);
+				// test if all task are unique
+				for (Iterator i = aSet.iterator() ; i.hasNext() ; ){
+					TaskDefinition t = ((TaskDescriptor)i.next()).getTaskDefinition();
+					if (t!=null){
+						for (int j = 0 ; j < v.size() ; j++){
+							System.out.println("\nID : \n" + t.getId() + "\n" + ((TaskDefinition)v.get(j)).getId() + "\nMemes ref : " + (t == ((TaskDefinition)v.get(j))));
+							assertTrue(
+								t.getId().equals(((TaskDefinition)v.get(j)).getId()) && t == ((TaskDefinition)v.get(j))
+								||
+								!t.getId().equals(((TaskDefinition)v.get(j)).getId())
+							);
+						}
+						if (!v.contains(t)){
+							v.add(t);
+						}
 					}
 				}
+			} catch (Exception e) {
+				System.out.println("Exception");
+				fail();
 			}
-		} catch (Exception e) {
-			System.out.println("Exception");
-			fail();
 		}
 	}
 
